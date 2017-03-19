27d25ca7ba92e27150a8ad3cc3b3884752a06c05
Nazar Gavryliuk <na3aga@gmail.com>
2017-03-19 15:29:29 +0200

    SIG-001. New Project

---
+++ app/src/main/java/whitechurchapplication/sig/View/AboutBCerkvaActivity.java
@@ -0,0 +1,24 @@
+package whitechurchapplication.sig.View;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import whitechurchapplication.sig.R;
+
+public class AboutBCerkvaActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_bcerkva);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
