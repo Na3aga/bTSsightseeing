42dbd729c0ddd03bc3d841ce2c494e4097f56fb0
viktor <dudkinviktor@gmail.com>
2017-03-22 19:40:29 +0200

    Add. SIG-11. Main screen MVP.

--- app/src/main/java/whitechurchapplication/sig/Presenter/asdasd.java
+++
@@ -1,8 +0,0 @@
-package whitechurchapplication.sig.Presenter;
-
-/**
- * Created by Nazar on 20.03.2017.
- */
-
-public class asdasd {
-}
---
+++ app/src/main/java/whitechurchapplication/sig/View/MainContract.java
@@ -0,0 +1,8 @@
+package whitechurchapplication.sig.View;
+
+/**
+ * Created by macbookpro on 22.03.17.
+ */
+
+public interface MainContract {
+}
