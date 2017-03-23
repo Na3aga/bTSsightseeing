93b23f39e08277a6728dc9082ab550ba11518823
Nazar Gavryliuk <na3aga@gmail.com>
2017-03-23 20:57:57 +0200

    Add. Multidex.

--- app/src/main/java/whitechurchapplication/sig/model/entities/Location.java
+++ app/src/main/java/whitechurchapplication/sig/model/entities/Location.java
@@ -1,26 +1,28 @@
-package whitechurchapplication.sig.model.entities;
-
-/**
- * Created by macbookpro on 22.03.17.
- */
-
-public class Location {
-
-
-
-    private void dossssssss() {
-        final Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                saveLocations();
-                in
-
-            }
-        });
-       thread.start();
-    }
-
-    private void dos() {
-
-    }
-}
+//package whitechurchapplication.sig.model.entities;
+//
+///**
+// * Created by macbookpro on 22.03.17.
+// */
+//
+//public class Location {
+//
+//
+//
+//    private void dossssssss() {
+//        final Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                saveLocations();
+//
+//            }
+//
+//            private void saveLocations() {
+//            }
+//        });
+//       thread.start();
+//    }
+//
+//    private void dos() {
+//
+//    }
+//}
